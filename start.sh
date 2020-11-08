b59c314a372756233f2cf037ec523d9baa4b5dbc
foreverloop <cjones5709@gmail.com>
2020-11-08 09:42:26 +0000

    amended startup script

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 app="docker.test"
 docker build -t ${app} .
-docker run -d -p 5039:80 \
-  --name=${app} \
-  -v $PWD:/app ${app}
+#docker run -d -p 5039:80 \
+#  --name=${app} \
+#  -v $PWD:/app ${app}
