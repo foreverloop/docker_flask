450f4d816d4e86c185c2f4bee95a6c266403f5c2
foreverloop <cjones5709@gmail.com>
2020-11-08 17:00:02 +0000

    amend groovy script

--- groovy_scripts/flask_groovy.groovy
+++ groovy_scripts/flask_groovy.groovy
@@ -1,6 +1,6 @@
 job('Flask_groovy_example') {
     scm {
-        git('https://github.com/foreverloop/docker_flask.git','/main') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/foreverloop/docker_flask.git','main') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
