ab4a16b5d79bf696f61ed069dadc840d5a264091
foreverloop <cjones5709@gmail.com>
2020-11-08 16:52:38 +0000

    amend groovy script now saved

--- groovy_scripts/flask_groovy.groovy
+++ groovy_scripts/flask_groovy.groovy
@@ -1,18 +1,11 @@
 job('Flask_groovy_example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/foreverloop/docker_flask.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
-    triggers {
-        scm('H/5 * * * *')
-    }
-    wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-    }
     steps {
-        shell("npm install")
+        shell("start.sh")
     }
 }
